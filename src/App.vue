d374577c6713465dc3ba5a8b33b940978c4f8a67
onton <programm.rechner@gmail.com>
2024-04-06 19:24:12 +0400

    add game component to app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
-<script setup></script>
+<script setup>
+import SimonGame from "./components/SimonGame.vue";
+</script>
 
 <template>
   <div id="app">
     <header>
       <div class="container">header</div>
     </header>
-
     <main>
-      <div class="container">main</div>
+      <div class="container">
+        <SimonGame />
+      </div>
     </main>
     <footer><div class="container">footer</div></footer>
   </div>
